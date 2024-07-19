1c47e87937873031da9df3fb8222e3d4740ceb50
Dilan Madusanka <madusankadilan226@gmail.com>
2024-07-19 11:11:21 +0530

    Search Bar Implement

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faBed, faCar, faTaxi} from "@fortawesome/free-solid-svg-icons"
+import {faBed, faCalendarDays, faCar, faPerson, faTaxi} from "@fortawesome/free-solid-svg-icons"
 
 function Header() {
   return (
@@ -44,6 +44,44 @@ function Header() {
          <h1 className="headerTtile">A lifeTime of Discount? It's Genius</h1>
          <p className="headerDescription">Lorem ipsum, dolor sit amet consectetur adipisicing</p>
          <button className='headerButton'>Sign in / Register</button>
+
+
+         <div className="headerSearch">
+            
+            <div className="headerSearchItem">
+
+                <FontAwesomeIcon icon={faBed} className='headerIcon' />
+
+                <input type="text" placeholder='Where are you going' className='headerSearchInput'/>
+            </div>
+
+
+            <div className="headerSearchItem">
+
+                <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+
+                <span className='headerSeachText'>  date to </span>
+
+            </div>
+
+
+
+            <div className="headerSearchItem">
+
+                <FontAwesomeIcon icon={faPerson} className='headerIcon' />
+                <span className='headerSeachText'> to adults 2 children 1 room</span>
+            </div>
+
+
+
+
+            <div className="headerSearchItem">
+                <button className='headerButton'>Search</button>
+                    
+            </div>
+
+
+         </div>
         </div>
     </div>
   )
