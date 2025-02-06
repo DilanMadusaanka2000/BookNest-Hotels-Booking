7996722fe459944aab32f472b7e3dfae96a15df5
Dilan Madusanka <madusankadilan226@gmail.com>
2025-02-06 11:57:18 +0530

    homePage design

---
+++ client/src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,66 @@
+import "./propertyList.css";
+
+const PropertyList = () => {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img
+         src="https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvdGVsfGVufDB8fDB8fHww"
+
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+           src="https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvdGVsfGVufDB8fDB8fHww"
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Apartments</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+           src="https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvdGVsfGVufDB8fDB8fHww"
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Resorts</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+           src="https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvdGVsfGVufDB8fDB8fHww"
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Villas</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+           src="https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvdGVsfGVufDB8fDB8fHww"
+          alt=""
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Cabins</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyList;
